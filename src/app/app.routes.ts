3bdbd1da758cedc89d31dfceba4dc3f75de71a43
Omar Shahbaz <omarshahbaz7@gmail.com>
2025-04-16 01:44:56 +0500

    Uploading to-do app

---
+++ src/app/app.routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from "@angular/router";
+
+import { routes as userRoutes } from "./users/users.route";
+import { NoTaskComponent } from "./tasks/no-task/no-task.component";
+import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+
+export const routes : Routes = [
+    {
+        path: '',
+        component: NoTaskComponent
+    },
+    {
+        path: 'users/:userId',
+        component: UserTasksComponent,
+        children: userRoutes,
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
+];
