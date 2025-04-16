c01eee5a4359deb040540f658ed43394fc6e89ca
Omar Shahbaz <omarshahbaz7@gmail.com>
2025-04-16 15:11:51 +0500

    Implementing sorting mechanism using signals

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, OnInit, signal } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
@@ -15,7 +15,8 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 export class TasksComponent  implements OnInit{
   userId = input.required<string>();
   // order = input<'asc' | 'desc'>();
-  order? : 'asc' | 'desc';
+  // order? : 'asc' | 'desc';
+  order = signal<'asc' | 'desc'>('desc');
   private tasksService = inject(TasksService);
   private activatedRoute = inject(ActivatedRoute);
   private destroyRef = inject(DestroyRef);
@@ -23,12 +24,19 @@ export class TasksComponent  implements OnInit{
   userTasks = computed(() =>
       this.tasksService.allTasks()
       .filter((task) => task.userId === this.userId())
+      .sort((a, b) => {
+        if(this.order() === 'desc'){
+          return a.id > b.id ? -1 : 1;
+        } else {
+          return a.id > b.id ? 1 : -1;
+        }
+      })
     );
 
     ngOnInit(): void {
       const subscription = this.activatedRoute.queryParams.subscribe({
         next : (params) => {
-          (this.order = params['order'])
+          (this.order.set(params['order']))
         }
       });
       this.destroyRef.onDestroy(() => subscription.unsubscribe());
